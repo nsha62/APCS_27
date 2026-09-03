ea7a740ef582e12bf93131477f3c124307ab5314
nsha62 <nrc3065@gmail.com>
2026-09-03 21:08:37 +0000

    completed labs 3 and 4

--- 000_Labs/004_String_class/base_code/starter.java
+++ 000_Labs/004_String_class/base_code/starter.java
@@ -7,8 +7,13 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String name = new String("Aliya");
+		String chorus = new String("Happy Birthday to you,");
+		String bridge = new String("Happy Birthday dear " + name);
+		System.out.println(chorus); 
+		System.out.println(chorus);
+		System.out.println(bridge);
+		System.out.println(chorus);
+
 	}
 }
--- 000_Labs/005_int/base_code/starter.java
+++ 000_Labs/005_int/base_code/starter.java
@@ -7,8 +7,6 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println(5*7/2%3-1);
 	}
 }
