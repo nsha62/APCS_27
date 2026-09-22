d8b5b8891d6e566e58e5a60f15557cf4ff431764
nsha62 <nrc3065@gmail.com>
2026-09-22 20:33:20 +0000

    update

--- 000_Labs/011_if_statements/base_code/starter.java
+++ 000_Labs/011_if_statements/base_code/starter.java
@@ -7,8 +7,10 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int num1 = 20;
+		int num2 = 17;
+		if(num1 < num2){
+			System.out.print(num1 + " is less than " +num2);
+		}
 	}
 }
